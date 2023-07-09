c395c7a0a77f2a76e10462cc8b81a4e59b76bf05
nur <nurmohammed840@gmail.com>
2023-07-10 00:14:37 +0600

    save

--- async-gen-macros/src/lib.rs
+++ async-gen-macros/src/lib.rs
@@ -1,64 +1,55 @@
 use proc_macro::*;
-use z::TokenStreamExt;
 
 #[proc_macro]
 pub fn gen_inner(input: TokenStream) -> TokenStream {
     let mut tokens = input.into_iter();
-    let crate_path = match tokens.next().unwrap() {
-        TokenTree::Group(group) => group.stream(),
-        _ => unimplemented!(),
-    };
+
+    let Some(TokenTree::Group(crate_path)) = tokens.next() else { unimplemented!() };
+    let crate_path = crate_path.stream();
 
     let mut has_yielded = false;
     let output = out(tokens, &mut has_yielded);
-    {
-        let mut s = TokenStream::new();
 
-        s.extend(crate_path.clone());
-        s.append_colon2();
-        s.append_ident("gen");
+    let mut o = TokenStream::new();
+    o.extend(crate_path.clone());
+    o.push_colon2();
+    o.push_ident("gen");
 
-        s.append({
-            let mut s = TokenStream::new();
-            s.append_punct('|');
-            s.append_ident("mut");
-            s.append_ident("yield_");
+    o.push_group(Delimiter::Parenthesis, |o| {
+        o.push_punct('|');
+        o.push_ident("mut");
+        o.push_ident("yield_");
 
-            if !has_yielded {
-                s.append_punct(':');
-                s.extend(crate_path);
-                s.append_colon2();
-                s.append_ident("Yield");
-            }
+        if !has_yielded {
+            o.push_punct(':');
+            o.extend(crate_path);
+            o.push_colon2();
+            o.push_ident("Yield");
+        }
 
-            s.append_punct('|');
-            s.append_ident("async");
-            s.append({
-                let mut s = TokenStream::new();
-                s.append_ident("let");
-                s.append_ident("v");
-                s.append_punct('=');
-                s.append_ident("async");
+        o.push_punct('|');
+        o.push_ident("async");
+        o.push_ident("move");
+        o.push_group(Delimiter::Brace, |o| {
+            o.push_ident("let");
+            o.push_ident("v");
+            o.push_punct('=');
+            o.push_ident("async");
 
-                s.append(output);
+            o.push(output);
 
-                s.append_punct('.');
-                s.append_ident("await");
-                s.append_punct(';');
-                s.append_ident("yield_");
-                s.append_punct('.');
-                s.append_ident("return_");
-                s.append({
-                    let mut s = TokenStream::new();
-                    s.append_ident("v");
-                    Group::new(Delimiter::Parenthesis, s)
-                });
-                Group::new(Delimiter::Brace, s)
+            o.push_punct('.');
+            o.push_ident("await");
+            o.push_punct(';');
+            o.push_ident("yield_");
+            o.push_punct('.');
+            o.push_ident("return_");
+            o.push_group(Delimiter::Parenthesis, |o| {
+                o.push_ident("v");
             });
-            Group::new(Delimiter::Parenthesis, s)
         });
-        s
-    }
+    });
+    o
 }
 
 fn out(mut tokens: token_stream::IntoIter, has_yielded: &mut bool) -> Group {
@@ -72,58 +63,61 @@ fn out(mut tokens: token_stream::IntoIter, has_yielded: &mut bool) -> Group {
                 for tt in &mut tokens {
                     match tt {
                         TokenTree::Punct(p) if p.as_char() == ';' => break,
-                        _ => expr.append(tt),
+                        _ => expr.push(tt),
                     }
                 }
-                o.append_ident("yield_");
-                o.append_punct('.');
-                o.append_ident("yield_");
-                o.append(Group::new(Delimiter::Parenthesis, expr));
-                o.append_punct('.');
-                o.append_ident("await");
-                o.append_punct(';');
+                o.push_ident("yield_");
+                o.push_punct('.');
+                o.push_ident("yield_");
+                o.push(Group::new(Delimiter::Parenthesis, expr));
+                o.push_punct('.');
+                o.push_ident("await");
+                o.push_punct(';');
             }
             TokenTree::Group(g) if g.delimiter() == Delimiter::Brace => {
-                o.append(out(g.stream().into_iter(), has_yielded));
+                o.push(out(g.stream().into_iter(), has_yielded));
             }
-            _ => o.append(tt),
+            _ => o.push(tt),
         }
     }
     Group::new(Delimiter::Brace, o)
 }
 
-mod z {
-    use super::*;
+trait TokenStreamExt {
+    fn push<U>(&mut self, token: U)
+    where
+        U: Into<TokenTree>;
 
-    pub trait TokenStreamExt {
-        fn append<U>(&mut self, token: U)
-        where
-            U: Into<TokenTree>;
-
-        #[inline]
-        fn append_punct(&mut self, ch: char) {
-            self.append(Punct::new(ch, Spacing::Alone))
-        }
-
-        #[inline]
-        fn append_ident(&mut self, name: &str) {
-            self.append(Ident::new(name, Span::call_site()))
-        }
-
-        #[inline]
-        fn append_colon2(&mut self) {
-            self.append(Punct::new(':', Spacing::Joint));
-            self.append(Punct::new(':', Spacing::Alone));
-        }
+    #[inline]
+    fn push_punct(&mut self, ch: char) {
+        self.push(Punct::new(ch, Spacing::Alone))
     }
 
-    impl TokenStreamExt for TokenStream {
-        #[inline]
-        fn append<U>(&mut self, token: U)
-        where
-            U: Into<TokenTree>,
-        {
-            self.extend(std::iter::once(token.into()));
-        }
+    #[inline]
+    fn push_ident(&mut self, name: &str) {
+        self.push(Ident::new(name, Span::call_site()))
+    }
+
+    #[inline]
+    fn push_group(&mut self, delimiter: Delimiter, f: impl FnOnce(&mut TokenStream)) {
+        let mut stream = TokenStream::new();
+        f(&mut stream);
+        self.push(Group::new(delimiter, stream))
+    }
+
+    #[inline]
+    fn push_colon2(&mut self) {
+        self.push(Punct::new(':', Spacing::Joint));
+        self.push(Punct::new(':', Spacing::Alone));
+    }
+}
+
+impl TokenStreamExt for TokenStream {
+    #[inline]
+    fn push<U>(&mut self, token: U)
+    where
+        U: Into<TokenTree>,
+    {
+        self.extend(std::iter::once(token.into()));
     }
 }
